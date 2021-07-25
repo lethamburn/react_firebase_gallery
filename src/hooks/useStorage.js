207d0af981a1379624c8c7fe8140980500ce333f
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-25 09:25:19 +0200

    FEAT useStorage, logics, errors and percent

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -9,5 +9,24 @@ const useStorage = (file) => {
   useEffect(() => {
     //referencias
     const storageRef = projectStorage.ref(file.name);
+
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      }
+    );
   }, [file]);
+
+  return { progress, url, error };
 };
+
+export default useStorage;
