da82f2b413508e06dbe3f99532574cf2841333b5
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-25 10:06:51 +0200

    FEAT ImageGrid

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>PhotoGram</h1>
+      <h1>PrivateGram</h1>
       <h2>P. Parker</h2>
       <p>Just a kid from Brooklyn</p>
     </div>
