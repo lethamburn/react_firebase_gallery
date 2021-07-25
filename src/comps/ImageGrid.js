d992f96ab25a48a5d5b5a80a92d8a95008e07c2f
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-25 10:49:21 +0200

    FEAT animations ImageGird and Progress Bar

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,15 +1,28 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
+import { motion } from "framer-motion";
 
-const ImageGrid = ({setSelectedImg}) => {
+const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore("images");
   return (
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div className="img-wrap" key={doc.id} onClick={() => setSelectedImg(doc.url)}>
-            <img src={doc.url} alt="uploaded picture" />
-          </div>
+          <motion.div
+            className="img-wrap"
+            key={doc.id}
+            layout
+            whileHover={{ opacity: 1 }}
+            onClick={() => setSelectedImg(doc.url)}
+          >
+            <motion.img
+              src={doc.url}
+              alt="uploaded picture"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{delay:1}}
+            />
+          </motion.div>
         ))}
     </div>
   );
