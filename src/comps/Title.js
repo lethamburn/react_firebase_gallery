d0b5484c376aec8c4913756e2e23bcad675230a9
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-25 08:44:53 +0200

    Initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>PhotoGram</h1>
+      <h2>P. Parker</h2>
+      <p>Just a kid from Brooklyn</p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD7oSxfA3pNRChXy67BlIIxcwgJQAjxHEk",
+  authDomain: "react-gallery-9bc03.firebaseapp.com",
+  projectId: "react-gallery-9bc03",
+  storageBucket: "react-gallery-9bc03.appspot.com",
+  messagingSenderId: "1081176245449",
+  appId: "1:1081176245449:web:4b2c35ce75d6861325b918",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
