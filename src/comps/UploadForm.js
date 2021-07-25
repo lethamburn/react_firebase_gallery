07a4fe5d784b0f78f7ac2c6c45f45316bbaf2557
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-25 09:19:07 +0200

    FIX Uploadform

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -8,17 +8,21 @@ const UploadForm = () => {
 
   const changeHandler = (e) => {
     let selected = e.target.files[0];
+
     if (selected && types.includes(selected.type)) {
       setFile(selected);
       setError("");
     } else {
       setFile(null);
-      setError("Please select an image file (png or jpeg");
+      setError("Please select an image file (png or jpg)");
     }
   };
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
